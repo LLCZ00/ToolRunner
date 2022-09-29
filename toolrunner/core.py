6684d81d6b93a3e499bc0fa74bf71bf4ba35d315
Clarence <37959893+LLCZ00@users.noreply.github.com>
2022-09-29 15:27:05 -0400

    Update core.py

--- toolrunner/core.py
+++ toolrunner/core.py
@@ -1,9 +1,18 @@
+#
+# Copyright (C) 2022 LLCZ00
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.  
+#
+
 """
-toolrunner v1.0.0
-Author: LLCZ00
-
-Drag and drop the target file to automatically run it through the defined tools.
-
+toolrunner.core
+Core class and methods for automating tools, commands, and processes with toolrunner.
 """
 
 import subprocess, sys, os   
